a1428419d5a70d14349931f0be4d895d46f1790c
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-08-06 16:40:51 -0300

    Atualizando o update do back e comentando o front

--- Front-end/book/src/app/book.service.ts
+++ Front-end/book/src/app/book.service.ts
@@ -3,29 +3,54 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from './book';
 
+/*
+É um decorador que marca a classe como um serviço que pode ser injetado em outros componentes ou serviços.
+*/
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' 
+  /*
+  Especifica que o serviço deve ser fornecido na raiz do aplicativo, o que significa que haverá uma única instância do serviço em toda a aplicação (serviço singleton).
+  */
 })
+
 export class BookService {
 
-  private apiUrl = 'http://localhost:8080/book';
+  private apiUrl = 'http://localhost:8080/book'; 
+  /*
+  É uma URL base para as chamadas HTTP que interagem com a API.
+  */
 
   constructor(private http: HttpClient) { }
+  /*
+  Um serviço Angular para fazer requisições HTTP. É injetado no construtor, permitindo que o BookService faça requisições HTTP para a API.
+  */
 
   createBook(book: Book): Observable<Book> {
     return this.http.post<Book>(this.apiUrl, book);
+    /*
+    Recebe um objeto Book que representa o livro a ser criado. Irá retornar um Observable que emite o livro criado. O tipo Book é esperado como resposta.
+    */
   }
 
   getBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.apiUrl);
+    /*
+    Irá retornar um Observable que emite um array de livros (Book[]).
+    */
   }
 
   updateBook(id: number, book: Book): Observable<Book> {
     return this.http.put<Book>(`${this.apiUrl}/${id}`, book);
+    /*
+    Recebe um id (identificador do livro) e um objeto Book com os novos dados. Irá retornar um Observable que emite o livro atualizado. 
+    */
   }
 
   deleteBook(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    /*
+    Recebe um id (identificador do livro a ser excluído). Irá retornar um Observable que não emite nenhum valor (tipo void), apenas indica que a exclusão foi realizada.
+    */
   }
 
   searchBooks(genre?: string, publisher?: string, language?: string): Observable<Book[]> {
@@ -41,5 +66,8 @@ export class BookService {
     }
 
     return this.http.get<Book[]>(`${this.apiUrl}/search`, { params });
+    /*
+    Os parâmetros genre, publisher, e language são parâmetros de busca opcionais. Irá retornar um Observable que emite um array de livros (Book[]) que correspondem aos critérios de pesquisa.
+    */
   }
 }
